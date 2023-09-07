b0372f1f5b2258dd1c07261239e9304a461cfa40
Hugo Vinícius <hugodossantos1998@gmail.com>
2023-09-07 20:36:11 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
-
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -18,9 +19,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
